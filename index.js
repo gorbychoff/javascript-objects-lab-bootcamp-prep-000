e0369f4cf6de7df90c0888e45ab5dbb742647cd0
Viam Mercer <viam.mercer@hotmail.com>
2018-08-21 17:43:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var obj2 = Object.assign({},object);
+  let obj2 = Object.assign({},object);
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
